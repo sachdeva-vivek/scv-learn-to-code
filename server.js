f7151008792e4509c3925bd1fbf8eb2c90ea28d8
Vivek Sachdeva <sachdeva-vivek@users.noreply.github.com>
2017-05-27 13:01:32 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,46 @@
 var express = require('express');
 var path = require('path'); // you could replace this var with const as well here.
 var bodyParser = require('body-parser');
-var MongoClient = require('mongodb').MongoClient
+var MongoClient = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID;
+var expressJwt = require('express-jwt');
+var jwt = require('jsonwebtoken');
+
+var secret = "secret";
+
 var app = express();
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-
-    next();
+  res.header('Access-Control-Allow-Origin', 'http://localhost');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  next();
 }
 
-app.use('/public', express.static(path.join(__dirname, 'public'))); // make the public folder static
+app.use(allowCrossDomain);
+
+app.use(expressJwt({
+  secret: secret,
+  credentialsRequired: false,
+  getToken: function fromHeaderOrQuerystring (req) {
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+        return req.headers.authorization.split(' ')[1];
+    } else if (req.query && req.query.token) {
+      return req.query.token;
+    }
+    return null;
+  }
+}));
+
+app.use('/api', expressJwt({secret: secret}));
+
+
+app.use(function(err, req, res, next) {
+  if (err.constructor.name === 'UnauthorizedError') {
+    res.status(401).send('Unauthorized');
+  }
+});
+//app.use('/public', express.static(path.join(__dirname, 'public'))); // make the public folder static
 app.use(bodyParser.urlencoded({extended: true})) // this is a middleware. it can modify request/response.
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(allowCrossDomain);
@@ -21,56 +49,78 @@ var db;
 
 app.get('/', function(req, res) {
   res.send("server is running fine.");
-  //res.header("Access-Control-Allow-Origin", "*");
-  //res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  //res.sendFile(__dirname + '/public/index8.html');
 });
 
-
-app.get('/items', function(req, res) {
-  db.collection('items').find().toArray(function(err, results) {
-
+app.get('/api/items', function(req, res) {
+  db.collection('items').find(
+    {
+      $or:[
+        {status:"pending"},
+        {status:"done"}
+      ]
+    }).toArray(function(err, results) {
     if (err) {
-      res.send("error occurred..")
+      res.send("error occurred.. "+err)
     } else {
-      //console.log(results);
       res.send(results);
     }
-
   })
 });
 
-app.delete('/item', function(req, res) {
-
-
-  db.collection('items').remove({_id: 5}, function(err, results) {
-    if (err) {
-      res.send("error occurred..")
-    } else {
-      //console.log(results);
-      res.send(results);
-    }
-  });
-
-
-
-});
-
-app.post('/item', function (req, res) {
-  console.log(req.body);
-  db.collection('items').save(req.body, function(err, result) {
+app.post('/api/item', function (req, res) {
+  db.collection('items').insert(req.body, function(err, result) {
+    console.log("result: "+result.ops[0]._id);
     if (err) {
       res.send("error...")
     } else {
-      res.send("success...");
+      res.send({_id: result.ops[0]._id, status: 'pending' });
     }
-    //console.log('saved to database')
-    //res.redirect('/')
   })
+});
+
+app.put('/api/item', function (req, res) {
+
+  db.collection('items').updateOne(
+      { "_id" : ObjectID(req.body._id) },
+      {
+        $set: { "status": req.body.status }
+      }, function(err, results) {
+        if (err) {
+          res.send("error...")
+        } else {
+          res.send({_id: req.body._id, status: req.body.status });
+        }
+   });
+
 
 });
 
-MongoClient.connect('mongodb://localhost:11501/todo-items', function(err, database) {
+ // signup -> test/test123 ->  test/asdfasdfasdfsadf+abcd
+ //login test/test1234 -> get me the user where user-name =  test
+ // do the bcrypt on test1234 .. is that equal to asdfasdfasdfsadf
+
+
+app.post('/login', function(req, res) {
+
+  if (req.body.userName !== "test" && req.body.password !== "test") {
+    res.writeHead(401, { 'Content-Type': 'application/json' });
+    res.end();
+  } else {
+    var userObject = {
+      first_name: 'Vivek',
+      last_name: 'Sachdeva',
+      email: 'vivek@test.com',
+      id: 123456
+    };
+    // We are sending the profile inside the token
+    var token = jwt.sign(userObject, secret, { expiresIn: 60*60 });
+    res.json({ token: token });
+  }
+});
+
+MongoClient.connect('mongodb://vivek:test123@localhost:11501/todo-items', function(err, database) {
+
+//MongoClient.connect('mongodb://localhost:11501/todo-items', function(err, database) {
 
   if (err) {
     console.console.log(err);
@@ -82,3 +132,13 @@ MongoClient.connect('mongodb://localhost:11501/todo-items', function(err, databa
   }
 
 });
+
+// db.createUser(
+//   {
+//     user: "admin",
+//     pwd: "admin",
+//     roles: [ { role: "userAdminAnyDatabase", db: "admin" } ]
+//   }
+// )
+//
+// db.createUser({ user: "vivek", pwd: "test123", roles: [{ role: "dbOwner", db: "todo-items" }] })
